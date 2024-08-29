c5df26decb0eed10bda683ee8aeb6b126fcdb34d
Carotorres26 <caritomiranda13@gmail.com>
2024-08-29 06:48:25 -0500

    arreglo de nav

---
+++ src/components/modules/tracking.jsx
@@ -0,0 +1,11 @@
+import React, {Component} from 'react'
+
+export default class Tracking extends Component{
+    render(){
+        return(
+            <div>
+                <h1>Tracking</h1>
+            </div>
+        )
+    }
+} 
