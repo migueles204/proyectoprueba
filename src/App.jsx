bafa1bedc4fd9b66c33a0027c8b3b809042370db
Carotorres26 <caritomiranda13@gmail.com>
2024-09-03 19:40:24 -0500

    arreglo dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import { useState } from 'react'; // Importa React
 import NavbarComp from './components/navegation';
-import Dashboard from './components/modules/dashboard'; // Importa el componente Dashboard
 
 function App() {
   return (
     <>
       <NavbarComp />
-      <Dashboard /> {/* Agrega el componente Dashboard */}
     </>
   );
 }
